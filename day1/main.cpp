e8216a8d566450e121a1dee9a0555f88e61dbbaa
Ionuț Leonte <ionut.leonte@gmail.com>
2019-12-01 10:58:00 +0200

    day1: complete

--- day1/main.cpp
+++ day1/main.cpp
@@ -1,3 +1,32 @@
+#include <fmt/format.h>
+#include <iostream>
+
+static inline long fuel_requirements_simple(long mass) {
+    return mass / 3 - 2;
+}
+
+static inline long fuel_requirements_complex(long mass) {
+    long total{0};
+    while (mass > 8) {
+        long delta = fuel_requirements_simple(mass);
+        total += delta;
+        mass = delta;
+    }
+    return total;
+}
+
 int main() {
+    long total_simple{0};
+    long total_complex{0};
+    long mass{0};
+
+    std::cin >> mass;
+    while (!std::cin.fail()) {
+        total_simple += fuel_requirements_simple(mass);
+        total_complex += fuel_requirements_complex(mass);
+        std::cin >> mass;
+    }
+    fmt::print("{}\n", total_simple);
+    fmt::print("{}\n", total_complex);
     return 0;
 }

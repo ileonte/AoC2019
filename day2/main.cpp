e1a2dffd6a08289ee879b261e9adf0176fad79bd
Ionuț Leonte <ionut.leonte@gmail.com>
2019-12-02 12:06:53 +0200

    day2: initial support work

--- day2/main.cpp
+++ day2/main.cpp
@@ -1,3 +1,21 @@
+#include <aoc.h>
+
+struct program_state {
+    std::vector<long> instructions;
+    size_t ip;
+};
+
+static inline struct program_state read_program() {
+    long v{};
+    std::vector<long> instructions;
+    while (std::cin >> v)
+        instructions.push_back(v);
+    return {std::move(instructions), 0};
+}
+
 int main() {
+    aoc::set_input_delimiters({','});
+    struct program_state ps = read_program();
+    fmt::print("{} {}\n", ps.instructions.size(), ps.instructions);
     return 0;
 }
---
+++ utils/aoc.h
@@ -0,0 +1,44 @@
+#pragma once
+
+#include <fmt/format.h>
+#include <iostream>
+#include <cstdint>
+#include <vector>
+#include <set>
+#include <locale>
+
+namespace fmt {
+    template <typename T>
+    struct formatter<std::vector<T>> {
+        template <typename ParseContext>
+        constexpr auto parse(ParseContext &ctx) { return ctx.begin(); }
+
+        template <typename FormatContext>
+        auto format(const std::vector<T> &v, FormatContext &ctx) {
+            format_to(ctx.out(), "[");
+            if (v.size() > 0) {
+                format_to(ctx.out(), "{}", v.at(0));
+                for (auto i = 1; i < v.size(); i++)
+                    format_to(ctx.out(), ", {}", v.at(i));
+            }
+            return format_to(ctx.out(), "]");
+        }
+    };
+}
+
+namespace aoc {
+    inline void set_input_delimiters(const std::vector<char>& delims) {
+        using cctype = std::ctype<char>;
+        const auto original_table = cctype::classic_table();
+        std::vector<cctype::mask> our_table(original_table, original_table + cctype::table_size);
+        for (auto& m : our_table)
+            m &= ~std::ctype_base::space;
+        for (auto c : delims)
+            our_table[c] |= std::ctype_base::space;
+        std::cin.imbue(std::locale(std::cin.getloc(), new cctype(our_table.data())));
+    }
+
+    inline void reset_input_delimiters() {
+        std::cin.imbue(std::locale());
+    }
+}

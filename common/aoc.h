0c70533a796f7ee7ba8590777d4e6fd2017d6aa3
Ionuț Leonte <ionut.leonte@gmail.com>
2019-12-18 03:14:30 +0200

    common/aoc.h: fix ASAN issues in aoc::trim()

--- common/aoc.h
+++ common/aoc.h
@@ -148,8 +148,8 @@ namespace aoc {
     }
 
     inline std::string_view trim(std::string_view sv) {
-        while (std::isspace(sv.front())) sv.remove_prefix(1);
-        while (std::isspace(sv.back())) sv.remove_suffix(1);
+        while (!sv.empty() && std::isspace(sv.front())) sv.remove_prefix(1);
+        while (!sv.empty() && std::isspace(sv.back())) sv.remove_suffix(1);
         return sv;
     }
 
